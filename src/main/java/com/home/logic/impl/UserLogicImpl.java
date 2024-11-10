bde67ef6010b2d654f0e3fe79434fefc05dc6c24
rahulbaliyan403 <rahulbaliyan403@gmail.com>
2024-11-10 16:16:23 +0530

    crud opration added.

--- src/main/java/com/home/logic/impl/UserLogicImpl.java
+++ src/main/java/com/home/logic/impl/UserLogicImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserLogicImpl implements UserLogic {
@@ -35,4 +36,47 @@ public class UserLogicImpl implements UserLogic {
        }
         return users;
     }
+
+    @Override
+    public User user(int id) {
+        Optional<UserEntity> userEntity= userDao.user(id);
+        User user = new User();
+        user.setId(userEntity.get().getUserId());
+        user.setName(userEntity.get().getUserName());
+        user.setPassword(userEntity.get().getUserPassword());
+        user.setEmail(userEntity.get().getUserEmail());
+        user.setSalary(userEntity.get().getUserSalary());
+        return user;
+    }
+
+    @Override
+    public void save(User user) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserName(user.getName());
+        userEntity.setUserPassword(user.getPassword());
+        userEntity.setUserEmail(user.getEmail());
+        userEntity.setUserSalary(user.getSalary());
+        userDao.save(userEntity);
+
+    }
+
+    @Override
+    public void update(int id, User user) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(id);
+        userEntity.setUserName(user.getName());
+        userEntity.setUserPassword(user.getPassword());
+        userEntity.setUserEmail(user.getEmail());
+        userEntity.setUserSalary(user.getSalary());
+        userDao.update(userEntity);
+
+    }
+
+    @Override
+    public void delete(int id) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(id);
+        userDao.delete(userEntity.getUserId());
+    }
+
 }
